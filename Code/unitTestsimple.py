483278e292f42b0ae6cd126e227494f9c4b7137d
Ale-Fon <90851040+Ale-Fon@users.noreply.github.com>
2025-03-27 15:08:57 -0500

    Update unitTestsimple.py

--- Code/unitTestsimple.py
+++ Code/unitTestsimple.py
@@ -1,13 +1,14 @@
 import unittest
-from game_logic import gameplay
+from game_logic import SimpleGame, GeneralGame
+
 
 class TestSOSGame(unittest.TestCase):
     def test_simple_mode(self):
-        game = gameplay(3, mode="Simple")
+        game = SimpleGame(3)
 
         # Make moves that lead to an SOS
         winner, _ = game.letterPlace(0, 0, 'S')
-        if winner:  # If the game already ended, assert the winner dynamically
+        if winner:  
             self.assertEqual(winner, game.current_turn, "First SOS should determine the winner.")
             return
         
